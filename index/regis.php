8b2adcfba813a078be8f5a96948ec4aefd8138ee
Suparerk <suparerk.tk@hotmail.com>
2018-11-01 15:49:43 +0700

    update

--- index/regis.php
+++ index/regis.php
@@ -2,49 +2,48 @@
   include_once 'head_.php';
 ?>
 <body>
-<style>
-	input[type="text"] 
-{
-    font-size:15px;
-}
-</style>
 <link href="../regis/cssregis.css" rel="stylesheet" type="text/css">
 
-
 <div>
-	<h1>REGISTERATION</h1>
-	<div class="left" >
-		<form class="SignUp" action="regisUser.php" method="POST">
-		FIRSTNAME <input type="text" name="Fname" style=" border-radius: 4px;">
-		<br><br><br>
-		LASTNAME <input type="text" name="Lname" style=" border-radius: 4px;">
-		<br><br><br>
-		Email <input type="Email" name="email" style="  border-radius: 4px;">
-		<br><br><br>
-		WEIGHT <input type="text" name="weight" style="border-radius: 4px;">
-		<br><br><br>
-		HEIGHT <input type="text" name="height" style="border-radius: 4px;">
-		<br><br><br>
-		AGE <input type="text" name="age" style="border-radius: 4px;">
-		<br><br><br>
-		SEX <input type="radio" name="sex" value="male">
-				<label>MALE</label> 
-		    <input type="radio" name="sex" value="female">
-				<label>FEMALE</label>
-		<br><br>
-		</form>
+<h1>REGISERATION</h1>
+<form class="SignUp" action="regisUser.php" method="POST">
+<div class="left" >
+FIRSTNAME <input type="text" name="Fname" style="border-radius: 4px;"><br><br>
+LASTNAME <input type="text" name="Lname" style="border-radius: 4px;"><br><br>
+E-MAIL <input type="email" name="email" style="border-radius: 4px;"><br><br>
+WEIGHT <input type="text" name="weight" style="border-radius: 4px;"><br><br>
+HEIGHT <input type="text" name="height" style="border-radius: 4px;"><br><br>
+AGE <input type="text" name="age" style="border-radius: 4px;"><br><br><br>
 </div>
 
-	<div class="right">
-	<form>
-	USERNAME <input type="text" name="username" 
-	style="border-radius: 4px;"><br><br><br>
-	PASSWORD <input type="password" name="password" 
-	style="border-radius: 4px;">
-	</form>
-</div>
+<div class="right">
+	USER ID <input type="text" name="username" style="border-radius: 4px;">
+	<br><br><br>
+	PASSWORD <input type="password" name="password" style="border-radius: 4px;">
+	<br><br><br>
+	SEX <input type="radio" name="sex" value="male"><label>MALE</label> 
+		<input type="radio" name="sex" value="female"><label>FEMALE</label>
+	</div>
 
-<input type="submit" name="nextPage" class="next"> NEXT>> </input>
+<input type="submit" name="nextPage" class="next" value="NEXT>>" 
+	style="	
+	font-size: 25px;
+	margin-top: 320px;
+	margin-left: 380px;
+	background-color:#4834d4;
+	color:white;
+	padding:25px;
+	border-radius: 8px;
+	font-weight: bold;
+	border: none;
+  	cursor: pointer;"> 	
+ </input>
+
+<br><br><br>
+
+</form>
+
+</div>
 
 <?php  
   include_once 'PopUpLogIn.php';
